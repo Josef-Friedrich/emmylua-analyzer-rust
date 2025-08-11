ea2a4185ef869a3aeb2020119181a3c248ef5c26
Josef Friedrich <josef@friedrich.rocks>
2025-08-11 17:28:01 +0200

    Write test

--- crates/emmylua_doc_cli/src/common.rs
+++ crates/emmylua_doc_cli/src/common.rs
@@ -20,3 +20,42 @@ pub fn render_const(typ: &LuaType) -> Option<String> {
         _ => None,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use internment::ArcIntern;
+
+
+    #[test]
+    fn test_render_const_none() {
+        // Should return None for non-const types
+        assert_eq!(render_const(&LuaType::Any), None);
+    }
+
+    #[test]
+    fn test_render_const_integer_const() {
+        assert_eq!(render_const(&LuaType::IntegerConst(42)), Some("42".to_string()));
+    }
+
+    #[test]
+    fn test_render_const_doc_integer_const() {
+        assert_eq!(render_const(&LuaType::DocIntegerConst(123)), Some("123".to_string()));
+    }
+
+    #[test]
+    fn test_render_const_float_const() {
+        assert_eq!(render_const(&LuaType::FloatConst(3.14)), Some("3.14".to_string()));
+    }
+
+    #[test]
+    fn test_render_const_string_const() {
+        let typ = LuaType::StringConst(ArcIntern::new(smol_str::SmolStr::new("hello")));
+        assert_eq!(render_const(&typ), Some("\"hello\"".to_string()));
+    }
+    // #[test]
+    // fn test_render_const_doc_string_const() {
+    //     let typ = LuaType::DocStringConst("world".into());
+    //     assert_eq!(render_const(&typ), Some("\"world\"".to_string()));
+    // }
+}
